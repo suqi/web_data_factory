68cd26bb10d79024fa8d658fe98095c6b24e6917
suqi <deqi.su@gmail.com>
2016-01-08 01:06:22 +0800

    add tutorial project code

--- src/diamond_bank/diamond_bank/items.py
+++ src/diamond_bank/diamond_bank/items.py
@@ -12,3 +12,8 @@ class DiamondBankItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class ExampleItem(scrapy.Item):
+    name = scrapy.Field()
+    population = scrapy.Field()
